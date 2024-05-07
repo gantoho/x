ef0df846d92aaf1e6acadb71e579226590b50f89
gantoho <i@ganto.me>
2024-05-07 18:04:34 +0800

    feat: update

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,25 @@
 package utils
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func Version() string {
 	return "v0.0.1"
+}
+
+func NewServer() *http.Server {
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+	err := server.ListenAndServe()
+	if err != nil {
+		return nil
+	}
+	return &server
 }
